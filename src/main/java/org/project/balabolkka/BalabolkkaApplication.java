b10e563d24ae84f52e14b5f9949c21eeb276b318
kkangh00n <kig2454@gmail.com>
2023-12-08 22:33:34 +0900

    Initial commit

---
+++ src/main/java/org/project/balabolkka/BalabolkkaApplication.java
@@ -0,0 +1,15 @@
+package org.project.balabolkka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BalabolkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalabolkkaApplication.class, args);
+	}
+
+}
