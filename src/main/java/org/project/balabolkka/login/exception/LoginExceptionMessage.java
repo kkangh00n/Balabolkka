84760fd91a0542de9bccba51497a95841e4f15f8
kkangh00n <kig2454@gmail.com>
2024-02-07 20:55:46 +0900

    refactor: 예외 메시지 enum 클래스 생성자 통일

--- src/main/java/org/project/balabolkka/login/exception/LoginExceptionMessage.java
+++ src/main/java/org/project/balabolkka/login/exception/LoginExceptionMessage.java
@@ -1,15 +1,13 @@
 package org.project.balabolkka.login.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum LoginExceptionMessage {
 
     WRONG_PASSWORD("아이디나 비밀번호가 틀립니다");
 
     private final String message;
-
-    LoginExceptionMessage(String message) {
-        this.message = message;
-    }
 }
